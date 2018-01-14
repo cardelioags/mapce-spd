d26d70bb0e4a1c4a80c3d8c18109701485005f3e
David Casillas <david.casillas@iea.edu.mx>
2018-01-13 23:53:23 -0600

    ok

--- src/app/administracion/main-administracion/main-administracion.component.ts
+++ src/app/administracion/main-administracion/main-administracion.component.ts
@@ -1,19 +1,23 @@
 import { Component, AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { TdMediaService } from '@covalent/core';
+import { MenusService } from "../../../services/menus.service";
+import { MenuPositionX } from '@angular/material/menu/typings/menu-positions';
 
 @Component({
-  changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-main-administracion',
   templateUrl: './main-administracion.component.html',
   styleUrls: ['./main-administracion.component.css'],
 })
 export class MainAdministracionComponent implements AfterViewInit {
 
+  adminMenu = [];
 
   constructor(private _changeDetectorRef: ChangeDetectorRef,
+    private _menu : MenusService,
     public media: TdMediaService) { }
-  
+    
     ngAfterViewInit(): void {
       this.media.broadcast();
+      this.adminMenu = this._menu.getAdminMenu();
     }
 }
--- src/services/menus.service.ts
+++ src/services/menus.service.ts
@@ -39,7 +39,51 @@ export class MenusService {
             title: 'Administración'
         }
     ]
+
+    adminMenu = [
+        {
+          icon: 'arrow_back',
+          title: 'Administración',
+          description: 'Panel principal de Administración',
+          link: '/administracion'
+        },
+        {
+          icon: 'people',
+          title: 'Personas',
+          description: 'Administrar el registro de personal',
+          link: '/administracion/personas'
+        },
+        {
+          icon: 'face',
+          title: 'Usuarios',
+          description: 'Administración de Cuentas de Usuarios',
+          link: '/administracion'
+        },
+        {
+          icon: 'verified_user',
+          title: 'Roles',
+          description: 'Administración de roles',
+          link: '/administracion'
+        },
+        {
+          icon: 'done_all',
+          title: 'Opciones',
+          description: 'Administra las opciones de menus y aplicación',
+          link: '/administracion'
+        },
+        {
+          icon: 'view_list',
+          title: 'Alcances',
+          description: 'Define y administra alcances para los roles',
+          link: '/administracion'
+        },
+     
+      ];
     getMenu(){
         return this.navMenu;
+    }
+
+    getAdminMenu(){
+        return this.adminMenu;
     }
 }
